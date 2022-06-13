caf0fc119080c4da00f9250ef8b24a0c1710f76f
Topias Mariapori <topias@mariapori.fi>
2022-06-13 21:51:44 +0300

    Program

---
+++ Ruokalistat.tk/Program.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ruokalistat.tk.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.SEOHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
+
+
+namespace Ruokalistat.tk
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+            builder.WebHost.UseUrls("http://*:6669");
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("all", policy => {
+                    policy.AllowAnyHeader().AllowAnyOrigin().Build();
+                });
+            });
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Digiruokalista API", Version = "v1" });
+            });
+            builder.Services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+            builder.Services.AddMvcCore().AddApiExplorer();
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+                app.UseForwardedHeaders();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+                app.UseForwardedHeaders();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors("all");
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            app.MapSwagger();
+
+            app.MapRazorPages();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "swagger";
+                c.SwaggerEndpoint("v1/swagger.json", "Digiruokalista API v1");
+                c.DefaultModelsExpandDepth(-1);
+                c.DefaultModelExpandDepth(-1);
+            });
+
+
+            app.UseXMLSitemap(app.Environment.ContentRootPath);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Identity/Account/Register", context => Task.Factory.StartNew(() => context.Response.Redirect("/Identity/Account/Login", true, true)));
+                endpoints.MapPost("/Identity/Account/Register", context => Task.Factory.StartNew(() => context.Response.Redirect("/Identity/Account/Login", true, true)));
+            });
+
+
+            app.Run();
+        }
+
+    }
+}
